24e2a5e69376e05c5e0f6d14ef4e956d1ebe49db
Rafael de Andrade <rafitoand@gmail.com>
2023-05-16 14:05:55 -0300

    Ajustes no menu mobile

--- script.js
+++ script.js
@@ -10,19 +10,22 @@ $(document).on("scroll",function(){
 // Abrir menu para dispositivos móveis
 function abrirMenuMobile() {
     $("#menus").addClass("navmobile");
-    $('.navmobile').css("animation", "deslizar 0.3s");
-    $('.navmobile').css("left", "0");
+    $(".navmobile").css("animation", "deslizar 0.3s");
+    $(".navmobile").css("left", "0");
+    $("body").css("overflow", "hidden");
     $("#lista").removeClass("menu").addClass("mobile"); 
     $("#fechar").removeClass("esconder").addClass("exibir");
     $("section").addClass("blur");
     $("footer").addClass("blur");
+    $("#blockpage").addClass("mostrar");
     $("#acoes").removeClass("botoes").addClass("bmobile");
 };
 
 // Fechar menu para dispositivos móveis
 function fecharMenuMobile() {
-    $('.navmobile').css("animation", "voltar 0.3s");
-    $('.navmobile').css("left", "-100%");
+    $(".navmobile").css("animation", "voltar 0.3s");
+    $(".navmobile").css("left", "-100%");
+    $("body").css("overflow", "auto");
 
     setTimeout(function(){
         $("#lista").removeClass("mobile").addClass("menu"); 
@@ -30,6 +33,7 @@ function fecharMenuMobile() {
         $("#fechar").removeClass("exibir").addClass("esconder");
         $("section").removeClass("blur");
         $("footer").removeClass("blur");
+        $("#blockpage").removeClass("mostrar");
         $("#acoes").removeClass("bmobile").addClass("botoes");
     }, 350);
 };
