f9b8eba0796db0c94ec3cff235745f9e4dd69fc0
Rafael de Andrade <rafitoand@gmail.com>
2023-05-16 12:22:54 -0300

    Correção do deslize do menu mobile

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Sombra do cabeçalho ao rolar
 $(document).on("scroll",function(){
-    if($(document).scrollTop()>30){
+    if($(document).scrollTop()>35){
         $("#cabecalho").removeClass("encaixado").addClass("sombreado"); 
     } else{
         $("#cabecalho").removeClass("sombreado").addClass("encaixado");
@@ -9,9 +9,10 @@ $(document).on("scroll",function(){
 
 // Abrir menu para dispositivos móveis
 function abrirMenuMobile() {
-    $("#lista").removeClass("menu").addClass("mobile"); 
     $("#menus").addClass("navmobile");
-    $("#box").removeClass("container");
+    $('.navmobile').css("animation", "deslizar 0.3s");
+    $('.navmobile').css("left", "0");
+    $("#lista").removeClass("menu").addClass("mobile"); 
     $("#fechar").removeClass("esconder").addClass("exibir");
     $("section").addClass("blur");
     $("footer").addClass("blur");
@@ -20,11 +21,15 @@ function abrirMenuMobile() {
 
 // Fechar menu para dispositivos móveis
 function fecharMenuMobile() {
-    $("#lista").removeClass("mobile").addClass("menu"); 
-    $("#menus").removeClass("navmobile");
-    $("#box").addClass("container");
-    $("#fechar").removeClass("exibir").addClass("esconder");
-    $("section").removeClass("blur");
-    $("footer").removeClass("blur");
-    $("#acoes").removeClass("bmobile").addClass("botoes");
+    $('.navmobile').css("animation", "voltar 0.3s");
+    $('.navmobile').css("left", "-100%");
+
+    setTimeout(function(){
+        $("#lista").removeClass("mobile").addClass("menu"); 
+        $("#menus").removeClass("navmobile");
+        $("#fechar").removeClass("exibir").addClass("esconder");
+        $("section").removeClass("blur");
+        $("footer").removeClass("blur");
+        $("#acoes").removeClass("bmobile").addClass("botoes");
+    }, 350);
 };
